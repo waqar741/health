6edf34751bb54f3d02d9437c58e4dfee2724d037
Waqar741 <ahmu741@gmail.com>
2025-09-29 19:11:35 +0530

    Started DRF- installation and other

--- hfp/memo/migrations/0001_initial.py
+++ hfp/memo/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             name='Memo',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('text', models.TextField(max_length=200)),
+                ('text', models.TextField(max_length=2000)),
                 ('photo', models.ImageField(blank=True, null=True, upload_to='photos/')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
--- hfp/memo/models.py
+++ hfp/memo/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Memo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField(max_length=200)
+    text = models.TextField(max_length=2000)
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
